5b7e27594cfa202c600e187cdb81a499f39595e8
alexey <alexeymazurik@gmail.com>
2016-03-19 00:48:58 +0200

    Changed public to dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp    		= require('gulp');
 
 gulp.task('html', function(){
   gulp.src('app/index.html')
-  	.pipe(gulp.dest('./public'));
+  	.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('images', function(){
   gulp.src('app/assets/images/*')
-  	.pipe(gulp.dest('./public/images'));
+  	.pipe(gulp.dest('./dist/images'));
 });
 
 gulp.task('watch', function(){
